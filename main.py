88be02cd831486c53b9ab6181410f09c1b0a0e42
aminhbl <amin.habibllh@gmail.com>
2021-06-10 10:55:27 +0430

    "feat: add MCdV"
    modify forward checking

--- main.py
+++ main.py
@@ -28,8 +28,19 @@ def MCdV(variables):
     return two_constrained_variables
 
 
-def MCgV(most_constrained_variables):
-    pass
+def MCgV(most_constrained_variables, unassigned_count):
+    most_constraining_variable = None
+    max_unassigned = -1
+    for var in most_constrained_variables:
+        unassigned = 0
+        x = var.x
+        y = var.y
+        unassigned += unassigned_count[0][x] - 1
+        unassigned += unassigned_count[1][y] - 1
+        if unassigned > max_unassigned:
+            max_unassigned = unassigned
+            most_constraining_variable = var
+    return most_constraining_variable
 
 
 def forward_checking(var, variables, unassigned_count):
@@ -78,40 +89,40 @@ def forward_checking(var, variables, unassigned_count):
 
     # < 000 and < 111 in each col and row
 
-    if x + 1 < variables.shape[0] and variables[x + 1][y] == var.value:
+    if x + 1 < variables.shape[0] and variables[x + 1][y].value == var.value:
         if x + 2 < variables.shape[0]:
             variables[x + 2][y].domain.remove(var.value)
         if x - 1 >= 0:
             variables[x - 1][y].domain.remove(var.value)
 
-    if x - 1 >= 0 and variables[x - 1][y] == var.value:
+    if x - 1 >= 0 and variables[x - 1][y].value == var.value:
         if x + 1 < variables.shape[0]:
             variables[x + 1][y].domain.remove(var.value)
         if x - 2 >= 0:
             variables[x - 2][y].domain.remove(var.value)
 
-    if x + 2 < variables.shape[0] and variables[x + 2][y] == var.value:
+    if x + 2 < variables.shape[0] and variables[x + 2][y].value == var.value:
         variables[x + 1][y].domain.remove(var.value)
 
-    if x - 2 >= 0 and variables[x - 2][y] == var.value:
+    if x - 2 >= 0 and variables[x - 2][y].value == var.value:
         variables[x - 1][y].domain.remove(var.value)
 
-    if y + 1 < variables.shape[1] and variables[x][y + 1] == var.value:
+    if y + 1 < variables.shape[1] and variables[x][y + 1].value == var.value:
         if y + 2 < variables.shape[1]:
             variables[x][y + 2].domain.remove(var.value)
         if y - 1 >= 0:
             variables[x][y - 1].domain.remove(var.value)
 
-    if y - 1 >= 0 and variables[x][y - 1] == var.value:
+    if y - 1 >= 0 and variables[x][y - 1].value == var.value:
         if y + 1 < variables.shape[1]:
             variables[x][y + 1].domain.remove(var.value)
         if x - 2 >= 0:
             variables[x][y - 2].domain.remove(var.value)
 
-    if y + 2 < variables.shape[1] and variables[x][y + 2] == var.value:
+    if y + 2 < variables.shape[1] and variables[x][y + 2].value == var.value:
         variables[x][y + 1].domain.remove(var.value)
 
-    if y - 2 >= 0 and variables[x][y - 2] == var.value:
+    if y - 2 >= 0 and variables[x][y - 2].value == var.value:
         variables[x][y - 1].domain.remove(var.value)
 
     # different cols and rows
@@ -121,36 +132,42 @@ def forward_checking(var, variables, unassigned_count):
             if unassigned_count[0][i] == 1:
                 equal = True
                 unassigned_variable = None
+                assigned_variable_value = None
 
                 for j in range(variables.shape[1]):
                     if variables[i][j].value is None:
                         unassigned_variable = variables[i][j]
+                        assigned_variable_value = variables[x][j].value
                     elif variables[x][j].value != variables[i][j].value:
                         equal = False
                         break
 
                 if equal:
-                    unassigned_variable.domain.remove(var.value)
+                    unassigned_variable.domain.remove(assigned_variable_value)
 
     if unassigned_count[1][y] == 0:
         for i in range(variables.shape[1]):
             if unassigned_count[1][i] == 1:
                 equal = True
                 unassigned_variable = None
+                assigned_variable_value = None
 
                 for j in range(variables.shape[0]):
                     if variables[j][i].value is None:
                         unassigned_variable = variables[j][i]
+                        assigned_variable_value = variables[j][y].value
                     elif variables[j][y].value != variables[j][i].value:
                         equal = False
                         break
 
                 if equal:
-                    unassigned_variable.domain.remove(var.value)
+                    unassigned_variable.domain.remove(assigned_variable_value)
 
 
 def MAC(var, variables):
-    pass
+    queue = []
+    x = var.x
+    y = var.y
 
 
 def main():
@@ -163,15 +180,17 @@ def input_parser():
     data = []
     unassigned_count = np.zeros((2, int(col)), dtype='int')
     for i in range(int(row)):
+        inner_data = []
         dummy = input().split()
         for j in range(int(col)):
             var = Variable(i, j)
-            data.append(var)
+            inner_data.append(var)
             if dummy[j] != "-":
                 var.value = int(dummy[j])
             else:
                 unassigned_count[1][j] += 1
                 unassigned_count[0][i] += 1
+        data.append(inner_data)
 
     return np.array(data, dtype='object'), unassigned_count
 
